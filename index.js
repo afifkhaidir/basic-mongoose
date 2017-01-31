e16d3b88856bbe15e19f4baf9b3f7e5cc9087134
afifkhaidir <afif.khaidir@gmail.com>
2017-01-31 23:28:30 +0700

    Connect to local DB, create POST function

--- index.js
+++ index.js
@@ -8,6 +8,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Bear = require('./app/models/bear');
+
+// connect to database
+mongoose.connect('mongodb://localhost:27017/mongoapi');
 
 // configure app to use body parser
 // this config will allow us to get the data from POST
@@ -22,10 +27,30 @@ var port = process.env.PORT || 8080;
 ===================*/
 var router = express.Router();
 
+// middleware
+router.use(function(req, res, next){
+	console.log('Request detected');
+	next();
+});
+
 // test route
 router.get('/', function(req, res) {
 	res.json({ message: 'Welcome to the family '});
 });
+
+router.route('/bears')
+	.post(function(req, res) {
+		var bear = new Bear();		// Create new instance of Bear Schema
+		bear.name = req.body.name;	// set bear name to name from request body
+	
+		// save the bear and check for errors
+		bear.save(function(err) {
+			if(err)
+				res.send(err);
+
+			res.json({ message: 'Bear created!' })
+		});
+	});
 
 
 /*==================
